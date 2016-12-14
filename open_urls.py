93b3111c01e3dd8cf9d5824945a689106fd7b9e7
Daniel Deng <ddeng5@uwo.ca>
2016-12-14 17:23:58 -0500

    run script with arguments in the form of: excelfile initialrow endrow

--- open_urls.py
+++ open_urls.py
@@ -1,15 +1,15 @@
 from openpyxl import load_workbook
 import webbrowser
+import sys
 
-
-workbook = load_workbook(filename = 'comprehensive-list.xlsx')
+workbook = load_workbook(filename = sys.argv[1])
 sheet = workbook['Sheet1']
 
 chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
 # Suppose your URLs are in column 5, rows 2 to 30
 
 url_column = "H"
-for row in range(1, 3):
+for row in range(int(sys.argv[2]), int(sys.argv[3])):
     string = url_column + str(row)
     url = sheet[string].value
     webbrowser.get(chrome_path).open(url)
