7e624fd68565045942320a213fc7bd9ea4aa6cfe
Daniel Deng <ddeng5@uwo.ca>
2017-01-01 17:30:33 -0500

    Creating question bank for interview prep

---
+++ questionBank.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+loggedIn = True
+
+
+def mainMenu():
+    print("Welcome to the Investment Banking Question Bank: ")
+    print("")
+    print("")
+    print("Which area would you like you like to be tested on: ")
+    print("     1. Understanding banking and suggested answers")
+
+    print("")
+    print("Enter q")
+    category = input()
+    print(category)
+
+
+def understandingBanking():
+    
+
+
+
+
+
+
+while (loggedIn):
+    mainMenu()
